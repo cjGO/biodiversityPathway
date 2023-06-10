cedb93950e8f7d5a6d3d15f7ab9b6b7ae17774ea
cjGO <cltngwn@gmail.com>
2023-06-10 06:47:56 +0000

    finish upload embeddings

--- project/app/routers.py
+++ project/app/routers.py
@@ -1,6 +1,7 @@
 # routers.py
 from tortoise.models import Model
 from fastapi import APIRouter, HTTPException
+from tortoise.exceptions import DoesNotExist, IntegrityError
 
 from .models import crud
 from .models.pydantic import EmbeddingPayloadSchema, ProteinPayloadSchema, ProteinResponseSchema, ProteinJSON, AminoAcidPayloadSchema, ProteinEmbeddingPayloadSchema
@@ -63,12 +64,63 @@ async def create_protein(payload: ProteinPayloadSchema):
     await protein.save()
     return None
 
-
 @router.post('/upload_embeddings/')
 async def upload_embedding(payload: EmbeddingPayloadSchema):
-    return payload
+    embedding_size = 2560
+    protein_id = payload.protein_id
+    model_name = payload.model_name
+    errors = ''
 
+    #confirm protein already exists in database
+    try:
+        protein = await Protein.get(id=protein_id)
+        sequence = protein.sequence
+    except DoesNotExist:
+        errors += "Protein with the provided id does not exist."
 
+    #confirm the amino acid embeddings haven't already been uploaded
+    try:
+        # Try to get the AminoAcid, if it exists an error will be thrown
+        amino_acid = await AminoAcid.get(protein_id=protein_id, location=1)
+        raise IntegrityError("Amino Acid at the provided location for the given protein id already exists.")
+    except DoesNotExist:
+        pass
+
+    #extract each amino acids individual embedding
+    aa_embeddings = payload.embedding_str.split()
+    aa_embeddings = [aa_embeddings[i:i+embedding_size] for i in range(0, len(aa_embeddings), embedding_size)]
+    for c,i in enumerate(aa_embeddings):
+        location = c
+        aa_embedding = i
+        aa = sequence[location]
+        print(c)
+        # create new AminoAcid entry
+        await AminoAcid.create(
+            amino_acid=aa, 
+            location=c, 
+            embeddings=aa_embeddings[c], 
+            protein_id=protein_id,
+        )
+    #convert the list of strings into 
+    #get protein embedding e.g. average of each aa sublist for each index
+    averages = [sum(float(val) for val in values) / len(values) for values in zip(*aa_embeddings)]
+    #convert back to a string and round decimals
+    protein_embedding = ' '.join(f'{num:.4f}' for num in averages)
+
+    # Try to create the ProteinEmbeddings entry
+    try:
+        await ProteinEmbeddings.create(
+            protein_id = protein_id,
+            model_name = model_name,
+            embeddings = protein_embedding,
+        )
+    except IntegrityError:
+        errors += "ProteinEmbeddings entry for the given protein and model already exists."
+
+    if errors:
+        return {"error": errors}
+    else:
+        return {'ok':'ok'}
 
 @router.get("/aa_embeddings/")
 async def get_aa_embeddings():
