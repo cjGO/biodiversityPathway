92d1470e39e9e843680113b86fca71c3667f0ed4
cjgo <cltngwn@gmail.com>
2023-06-13 02:09:26 +0000

    added cors

--- project/app/main.py
+++ project/app/main.py
@@ -3,6 +3,7 @@ import os
 from fastapi import FastAPI, Depends
 from tortoise.contrib.fastapi import register_tortoise
 from .routers import router
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.config import get_settings, Settings
 
@@ -10,6 +11,13 @@ from app.config import get_settings, Settings
 app = FastAPI()
 app.include_router(router)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 register_tortoise(
     app,
